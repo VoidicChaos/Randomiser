ebe21d33285398d37cf47f0676ff14b3d7e234c4
VoidicChaos <touinstallation@gmail.com>
2026-04-12 20:13:48 +0800

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,16 +1,24 @@
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using MiraAPI.PluginLoading;
 
 namespace RandomiserTOUM
 {
-    [BepInPlugin("com.chaos.randomiser", "Mira Randomiser", "1.0.0")]
-    public class Plugin : BasePlugin
+    [BepInPlugin("chaos.randomiser", "Mira Randomiser", "1.0.1")]
+    [BepInDependency("auavengers.tou.mira")]
+    [BepInDependency("mira.api")]
+    public class Plugin : BasePlugin, IMiraPlugin
     {
         public static Harmony HarmonyInstance;
+        public string ID => "com.colin.randomiser";
+        public string Name => "Mira Randomiser";
+        public string OptionsTitleText => "Mira Randomiser";
+        public ConfigFile GetConfigFile() => Config;
         public override void Load()
         {
-            HarmonyInstance = new Harmony("com.chaos.randomiser");
+            HarmonyInstance = new Harmony("chaos.randomiser");
             HarmonyInstance.PatchAll();
         }
     }
