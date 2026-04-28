a73dba5bc8542a1ebdcc393fc5eb5358e986cd45
VoidicChaos <touinstallation@gmail.com>
2026-04-28 20:32:07 +0800

    v1.0.1
    Updated Plugin.cs so that it can show in the main menu.

--- Plugin.cs
+++ Plugin.cs
@@ -3,23 +3,31 @@ using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using MiraAPI.PluginLoading;
+using Reactor;
+using Reactor.Utilities;
 
 namespace RandomiserTOUM
 {
-    [BepInPlugin("chaos.randomiser", "Mira Randomiser", "1.0.0")]
+    [BepInPlugin("chaos.randomiser", "Mira Randomiser", "1.0.1")]
     [BepInDependency("auavengers.tou.mira")]
     [BepInDependency("mira.api")]
-    public class RandomiserPlugin : BasePlugin, IMiraPlugin
+    public class MiraRandomiser : BasePlugin, IMiraPlugin
     {
         public static Harmony HarmonyInstance;
         public string ID => "chaos.randomiser";
         public string Name => "Mira Randomiser";
         public string OptionsTitleText => "Randomiser";
         public ConfigFile GetConfigFile() => Config;
+
         public override void Load()
         {
+            Log.LogInfo($"MiraRandomiser v1.0.1 loading...");
+
             HarmonyInstance = new Harmony("chaos.randomiser");
             HarmonyInstance.PatchAll();
+
+            Log.LogInfo("MiraRandomiser Loaded.");
+            ReactorCredits.Register<MiraRandomiser>(ReactorCredits.AlwaysShow);
         }
     }
 }
